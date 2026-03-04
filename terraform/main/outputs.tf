7f1d867caefac2fa52f7be40a79525f16a0f1cd9
Robert076 <robertberes06@gmail.com>
2026-03-04 15:47:54 +0200

    feat: removed some terraform config files for now

--- terraform/main/outputs.tf
+++ terraform/main/outputs.tf
@@ -5,23 +5,3 @@ output "documents_s3_bucket_name" {
 output "s3_doclane_role" {
   value = module.s3.s3_doclane_role
 }
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnet_arn" {
-  value = module.vpc.public_subnet_arn
-}
-
-output "public_subnet_id" {
-  value = module.vpc.public_subnet_id
-}
-
-output "private_subnet_arn" {
-  value = module.vpc.private_subnet_arn
-}
-
-output "private_subnet_id" {
-  value = module.vpc.private_subnet_id
-}
--- terraform/modules/frontend/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
-output "frontend_bucket_name" {
-  value = aws_s3_bucket.frontend.id
-}
