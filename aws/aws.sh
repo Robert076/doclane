3a86d38eca561968c66aebcfe9e5c002061ddbf2
Robert076 <robertberes06@gmail.com>
2026-04-22 14:38:06 +0300

    feat: backend infra is almost done

--- aws/aws.sh
+++ aws/aws.sh
@@ -1,1 +1,7 @@
-aws cloudformation create-stack --stack-name doclane-vpc --template-body file://vpc.yml --parameters ParameterKey=AZ1,ParameterValue=eu-west-1a ParameterKey=AZ2,ParameterValue=eu-west-1b
+aws cloudformation create-stack --stack-name doclane-vpc --template-body file://vpc.yml --parameters ParameterKey=AZ1,ParameterValue=eu-west-1a ParameterKey=AZ2,ParameterValue=eu-west-1b ParameterKey=MyPublicCidr,ParameterValue=/32
+
+aws cloudformation create-stack --stack-name doclane-rds-params --template-body file://rds-params.yml --parameters ParameterKey=DBPassword,ParameterValue=robert ParameterKey=DBUsername,ParameterValue=robertrobert
+
+aws cloudformation create-stack --stack-name doclane-rds --template-body file://rds.yml 
+
+aws cloudformation create-stack --stack-name doclane-bastion --template-body file://bastion-host.yml
